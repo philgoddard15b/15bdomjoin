a8c42527fde37b0ca3957efae10914b6cd204f85
philgoddard15b <115150475+philgoddard15b@users.noreply.github.com>
2022-12-16 10:48:38 +0000

    Delete join.ps1

--- join.ps1
+++
@@ -1,9 +0,0 @@
-$ip = read-host "IP Address to set"
-$adapter = get-netadapter | select -expand ifindex
-
-New-netipaddress -ipaddress $ip -defaultgateway 10.25.25.1 -prefixlength 24 -interfaceindex $adapter
-Set-dnsclientserveraddress -interfaceindex $adapter -serveraddresses 10.25.25.26,192.168.1.5
-
-$name = read-host "Enter computer name"
-
-Add-Computer -DomainName "15below.local" -OUPath "OU=BuildApp Servers,OU=RapidSwitch,DC=15below,DC=local" -newname $name credentials (get-credential) -server btn-dc04.15below.local
